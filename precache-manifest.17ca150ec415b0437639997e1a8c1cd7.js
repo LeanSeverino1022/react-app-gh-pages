c940ea7add2994a5c422344920288efb500fb7ab
Puyih Severino <yihseverino@gmail.com>
2019-01-18 11:06:25 +0800

    Updates

---
+++ precache-manifest.17ca150ec415b0437639997e1a8c1cd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-gh-pages1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4b54bfdf860f20544b90",
+    "url": "/react-app-gh-pages1/static/js/runtime~main.4b54bfdf.js"
+  },
+  {
+    "revision": "f5d3d5282134e37b6b38",
+    "url": "/react-app-gh-pages1/static/js/main.f5d3d528.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-app-gh-pages1/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f5d3d5282134e37b6b38",
+    "url": "/react-app-gh-pages1/static/css/main.a6e28d68.chunk.css"
+  },
+  {
+    "revision": "f1876c01c38fa176c3280f778e1716e7",
+    "url": "/react-app-gh-pages1/index.html"
+  }
+];
